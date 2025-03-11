c0862f81ffbe40b1f6a21df061cd746ecd938dbc
Vaclav Pavlin <vaclav.pavlin@gmail.com>
2025-03-11 19:32:06 +0100

    Fix a typo
    Co-authored-by: Ivan FB <128452529+Ivansete-status@users.noreply.github.com>

--- waku/options.go
+++ waku/options.go
@@ -4,7 +4,7 @@ import "github.com/waku-org/waku-go-bindings/waku/common"
 
 type WakuNodeOption func(*WakuNode)
 
-// This allows you to pass arbitrary valid config options to Nwaku baed on https://github.com/waku-org/nwaku/blob/master/waku/factory/external_config.nim
+// This allows you to pass arbitrary valid config options to Nwaku based on https://github.com/waku-org/nwaku/blob/master/waku/factory/external_config.nim
 // It is mostly for development and experimental purposes and will be removed in the future.
 func WithExtraOptions(extraOptions common.ExtraOptions) WakuNodeOption {
 	return func(wn *WakuNode) {
